d93ab7a30f617bdd5246b45a0b5502c49af39397
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-23 13:20:36 -0300

    feat: implementando a logica do contador nos botões

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Counter(props) {
 
+    const [count, setCount] = useState(0)
+
+
     return (
         <div>
-            <div>0</div>
-            <button>-</button>
-            <button>+</button>
+            <div>{count}</div>
+            <button onClick={() => {setCount(prev => prev-1)}}>-</button>
+            <button onClick={() => {setCount(prev => prev+1)}}>+</button>
         </div>
     )
 }
