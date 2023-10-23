bb189ddcf892293223d2f6a69c7c54e22f9fa2aa
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-23 13:40:34 -0300

    implementando o CounterProvider no App, englobando todos os componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Counter from './components/Counter';
 import Header from './components/Header';
+import { CounterProvider } from './context/CounterContext';
 
 function App() {
   return (
     <div className='App'>
+      <CounterProvider>
         <Header />
         <Counter />
+      </CounterProvider>
+
     </div>
   );
 }
