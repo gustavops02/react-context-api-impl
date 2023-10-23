0be7d15449f7fc6d012f1c3d2affd1a994021e31
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-23 13:17:06 -0300

    feat: criação do novo componente Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Counter(props) {
+
+    return (
+        <div>
+            <div>0</div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+}
