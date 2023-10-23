1a80f4056e8b4b47e73cb47d7f471df3da4a49e6
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-10-23 13:26:25 -0300

    feat: Dando estilo ao Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.css'
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <h3>Contador</h3>
+            <div>0</div>
+        </div>
+    )
+}
